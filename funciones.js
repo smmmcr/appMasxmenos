f4f3e181cfd486054cabc3d4c906a7e999d59bae
Servicios de mercadeo <ingenieria@movilmultimediasa.com>
2013-10-07 09:09:46 -0600

    3.0

--- funciones.js
+++ funciones.js
@@ -9,7 +9,7 @@ var fileSystem = {};
 document.addEventListener("deviceready", onDeviceReady, false);
  function onDeviceReady() {
 //Inicializamos las BD
-//checkConnection();
+checkConnection();
     }
 
 window.addEventListener('load', function() {
@@ -23,7 +23,7 @@ $(document).one("mobileinit", function () {
 	$.mobile.defaultDialogTransition = 'slide';*/
 	$.mobile.defaultPageTransition = 'none';
 	$("#cargaimg" ).show();
-	appDB();
+//	appDB();
 
 });
 	 function checkConnection() {
